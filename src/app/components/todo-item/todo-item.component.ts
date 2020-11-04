155a243e6f988242634692e561ef977b26385cdd
Dru Serkes <DruSerkes@gmail.com>
2020-11-04 01:12:12 -0500

    add event emitter

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import { Todo } from 'src/app/models/Todo';
 
@@ -9,7 +9,8 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
-
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+  
   constructor(private TodoService: TodoService) { }
 
   ngOnInit(): void {
@@ -32,7 +33,7 @@ export class TodoItemComponent implements OnInit {
   }
 
   onDelete(todo) {
-    console.log('deleted')
+    this.deleteTodo.emit(todo);
   }
 
 }
